3f3cf467d29431ba51ea6628debc9702545ed4ce
Joel R <joel@ethermious.com>
2025-11-23 01:31:52 -0600

    dash update and other updates

--- sleep_monitoring/dash_app/app.py
+++ sleep_monitoring/dash_app/app.py
@@ -217,4 +217,4 @@ def update_review(sleep_date_value, threshold, min_duration):
 
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=8050, debug=False)
+    app.run(host="0.0.0.0", port=8050, debug=False)
